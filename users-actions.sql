653a883ae43979dcaf02f3997306eb113a281559
Thomas K <thmskffr@gmail.com>
2022-05-13 13:24:20 +0200

    final database model

--- users-actions.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `users_actions` (
-  `user_id` INT,
-  `action_id` INT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-ALTER TABLE `users_actions` ADD FOREIGN KEY (`user_id`) REFERENCES `user_profiles`(`id`) ON DELETE SET NULL;
-ALTER TABLE `users_actions` ADD FOREIGN KEY (`action_id`) REFERENCES `actions`(`id`) ON DELETE SET NULL;
