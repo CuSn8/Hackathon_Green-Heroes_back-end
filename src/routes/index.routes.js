254754cf2a64bc92a29e2987418d8c7711635611
Thomas K <thmskffr@gmail.com>
2022-05-13 07:54:55 +0200

    myprofile routes created

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,7 @@ const usersRouter = require('./user.routes');
 const animalsRouter = require('./animal.routes');
 const actionsRouter = require('./actions.routes');
 const scrapingRouter = require('./scraping.routes');
+const myProfileRouter = require('./myprofile.routes');
 const cors = require('cors');
 const session = require('express-session');
 
@@ -11,28 +12,29 @@ const session = require('express-session');
 router.use(cors({
     origin: "http://localhost:3000",
     credentials: true
-  }));
-    
-  router.use(session({
-      secret: "secret",
-      resave: false,
+}));
+
+router.use(session({
+    secret: "secret",
+    resave: false,
     saveUninitialized: false,
     rolling: true,
     cookie: {
         httpOnly: false,
-      sameSite: "lax",
-    secure: false
+        sameSite: "lax",
+        secure: false
     }
-    }));
-  
-  router.use(function (req, res, next) {
-      req.session.test = "test";
-      next();
-    });
+}));
+
+router.use(function (req, res, next) {
+    req.session.test = "test";
+    next();
+});
 
 router.use('/users', usersRouter);
 router.use ('/animals', animalsRouter);
 router.use ('/actions', actionsRouter);
 router.use ('/news', scrapingRouter);
+router.use('/myprofile', myProfileRouter);
 
 module.exports = router;
